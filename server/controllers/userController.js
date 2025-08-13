7888ed5f0a863bb506832d3020845f983aa3c149
satvik00007777 <satvik2018007@gmail.com>
2025-08-13 17:54:15 +0530

    Added the controller

---
+++ server/controllers/userController.js
@@ -0,0 +1,41 @@
+import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: 'Please provide all fields' });
+        }
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            });
+        } else {
+            res.status(500).json({ message: 'Server Error', error: error.message });
+        }
+    } catch(error) {
+
+    }
+};
+
+export default { registerUser };
