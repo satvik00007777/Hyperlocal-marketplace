5541947a64789f684ae5e70ffe2decd3bacdac0f
satvik00007777 <satvik2018007@gmail.com>
2025-08-14 12:25:05 +0530

    protected route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const registerUser = async (req, res) => {
     try {
@@ -34,9 +35,42 @@ const registerUser = async (req, res) => {
             res.status(500).json({ message: 'Server Error', error: error.message });
         }
     } catch(error) {
-        console.error(error); // Log the error to the console
+        console.error(error);
         res.status(500).json({ message: 'Server Error' });
     }
 };
 
-export { registerUser };
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if(user && (await bcrypt.compare(password, user.password))) {
+            res.status(200).json({ 
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                token: generateToken(user._id)
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch(error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+    }
+}
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+};
+
+const getUserProfile = async (req, res) => {
+    res.status(200).json(req.user);
+}
+
+export { registerUser, loginUser, getUserProfile };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { registerUser } from '../controllers/userController.js';
+import { registerUser, loginUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+router.get('/me', protect, getUserProfile);
 
 export default router;
